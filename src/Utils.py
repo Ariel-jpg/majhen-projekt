789b17e0db2cae235c2fff377b7ab37f77993bd3
PIC16F88 <epofmip@gmail.com>
2021-10-02 12:43:29 -0300

    Initial commit

---
+++ src/Utils.py
@@ -0,0 +1,8 @@
+# "Static" class that will be used as a "helper" for common tasks.
+# If you want to do tests, you can do it with this class.
+
+
+class Utils:
+    @staticmethod
+    def method_1():
+        pass
